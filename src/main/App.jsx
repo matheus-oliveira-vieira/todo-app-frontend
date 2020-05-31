7cacc1fb980b1ec836665fbe23857300810d18b3
Matheus Oliveira <matheus.oliveirav@gmail.com>
2020-05-31 10:18:37 -0300

    Initial configuration

---
+++ src/main/App.jsx
@@ -0,0 +1,19 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+import React from 'react'
+export default props => {
+return (
+<div className='container'>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+<h1>Test</h1>
+</div>
+)
+}
