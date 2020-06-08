39f98c8ff0b6ddfc0abb8d115231e617c204b711
Matheus Oliveira <matheus.oliveirav@gmail.com>
2020-06-08 11:20:43 -0300

    Finishing front-end

--- src/template/IconButton.jsx
+++ src/template/IconButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import If from './If'
 export default props => {
-    if(props.hide){
-        return null
-    }else{
-        return (
+    
+    return (
+        <If test={!props.hide}>
             <button className={'btn btn-' + props.style} onClick={props.onClick}>
-                <i className={'fa fa-' + props.icon}></i>
+                <i className={'fa fa-' + props.icon}></i>           
             </button>
+        </If>
         )
     }
-}
--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -2,13 +2,27 @@ import React from 'react'
 import Grid from '../template/Grid'
 import IconButton from '../template/IconButton'
 export default props => {
-    return (
+  const keyHandler = (e) => {
+    if(e.key === 'Enter'){
+      e.shiftKey ? props.handleSearch() : props.handleAdd() 
+    }else if(e.key === 'Escape'){
+      props.handleClear()
+    }
+  }  
+  
+  return (
         <div role='form' className='todoForm'>
             <Grid cols='12 9 10'>
-                <input id='description' className='form-control' placeholder='Add task here'></input>
+                <input id='description' 
+                    className='form-control' placeholder='Add task here' 
+                    onChange={props.handleChange} 
+                    onKeyUp={keyHandler}
+                    value={props.description}></input>
             </Grid>
             <Grid cols='12 3 2'>
-                <IconButton style='success' icon='plus'></IconButton>
+                <IconButton style='success' icon='plus' onClick={props.handleAdd}></IconButton>
+                <IconButton style='info' icon='search' onClick={props.handleSearch}></IconButton>
+                <IconButton style='default' icon='close' onClick={props.handleClear}></IconButton>
             </Grid>
         </div>  
     )
